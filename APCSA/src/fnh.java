98fdc78a3dcc864110b6fdbae1ca52907a545d4b
bartschmackz <bartschmackz@BP0105054211P00>
2020-02-25 09:05:43 -0500

    nice!

--- APCSA/src/fnh.java
+++ APCSA/src/fnh.java
@@ -1,4 +1,27 @@
 
 public class fnh {
 
+	public static void main(String args[]) {
+		System.out.println("prepare for greatness");
+boolean name = true;
+		
+			for(int i = 0; i < 30; i++)
+			{
+				for(int j= 0; j < i; j++) {
+					if(name) {
+						System.out.print("john ");
+						name = false;
+					}else {
+						System.out.print("wall ");
+						name = true;
+					}
+					
+				}
+				System.out.println();
+			}
+			
+	
+			
+	}
+
 }
